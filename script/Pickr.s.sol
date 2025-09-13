3ad47e67208f4c11d2b5c6c1fa9b73ce90a0a79e
Febry Ardiansyah <febrymuhammad80@gmail.com>
2025-09-13 13:51:20 +0700

    init

---
+++ script/Pickr.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {Pickr} from "../src/Pickr.sol";
+
+contract DeployPickrScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        new Pickr();
+        vm.stopBroadcast();
+    }
+}
